11748dc462f11a9faad56f54b3a1e3e8419bc108
Ronan Kelly <Kelly-R95@ulster.ac.uk>
2024-10-08 10:09:55 +0100

    Initial Commit

--- Week3-Library/Program.cs
+++ Week3-Library/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Book
+{
+    string Title;
+    string Author;
+    string ISBN;
+    int NoOfPages;
+
+    void DisplayInfo()
+    {
+
+        // output the book information to the console
+        Console.WriteLine("Book information");
+        Console.WriteLine("----------------");
+        Console.WriteLine($"Book.Title: {Title}");
+        Console.WriteLine($"Book author: {Author}");
+        Console.WriteLine($"Book ISBN: {ISBN}");
+        Console.WriteLine($"Book Length {NoOfPages}");
+        Console.WriteLine();
+
+    }
+
+    static void Main(string[] args)
+    {
+        //Create a New instance of the book class
+        Book book = new Book();
+        book.Title = "C# for beginners";
+        book.Author = "Bill Gates";
+        book.ISBN = "123456789";
+        book.NoOfPages = 200;
+
+        
+
+        Book book2 = new Book();
+        book2.Title = "C# methods and classes";
+        book2.Author = "Microsoft";
+        book2.ISBN = "987654321";
+
+        //output book info to the console
+
+        book.DisplayInfo();
+        book2.DisplayInfo();
+
+        
+    }
+}
